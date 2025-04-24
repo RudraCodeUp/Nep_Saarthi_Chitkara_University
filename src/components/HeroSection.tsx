ec988299ea7af9ee4c86c37749f03a2d75a0f486
RudraCodeUp <141010176+RudraCodeUp@users.noreply.github.com>
2025-04-24 19:41:28 +0530

    images added

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -65,7 +65,7 @@ const HeroSection = () => {
               <div className="w-full h-full max-w-md mx-auto">
                 <div className="relative z-10 animate-float">
                   <img 
-                    src="https://i.imgur.com/FaTnTJa.png" 
+                    src="E:\nep-saarthi\images\Natinal-Education-Policy-2020-1170x720.png"
                     alt="NEP 2020 Illustration" 
                     className="w-full h-auto rounded-lg shadow-2xl"
                   />
